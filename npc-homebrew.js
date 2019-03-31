b35a72fdff8db2946555007feaec4ba5608f5fff
Muahahayes <muahahayes@gmail.com>
2019-03-31 15:46:33 -0600

    reworded homebrew's usage

--- npc-homebrew.js
+++ npc-homebrew.js
@@ -8,7 +8,7 @@ let npc = require('./npc-source.js')
 
 if (process.argv[2] && process.argv[2].includes('h')) {
     console.log('---------------------------------------------------' +
-                '\n A D&D 5e character generator.' + 
+                '\n A D&D 5e homebrew character generator.' + 
                 '\n Usage: node npc-homebrew.js # # c r s' +
                 '\n No arguments will give a random character lv 1-20.' +
                 '\n Arguments:' +
